ff2c5f55f4610beffbe3bf38962dcb832e9a5032
Scott Yang <scogranger@gmail.com>
2022-03-31 23:36:46 +1300

    commit message

--- app.js
+++ app.js
@@ -23,6 +23,16 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./worksheet3_app/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./worksheet3_app/build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
