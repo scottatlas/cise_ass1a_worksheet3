1a52eaa08f10d0180bc9b017bbc7f2a79fd2a11b
Scott Yang <scogranger@gmail.com>
2022-04-01 20:04:53 +1300

    fix2

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
